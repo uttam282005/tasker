e07075aeb7b479796e0266359a5ee7574a4eef68
uttam282005 <uttam282005@gmail.com>
2026-01-11 19:05:13 +0530

    feat(service): add comment and category service

--- internal/service/services.go
+++ internal/service/services.go
@@ -7,15 +7,21 @@ import (
 )
 
 type Services struct {
-	Auth *AuthService
-	Job  *job.JobService
+	Auth     *AuthService
+	Job      *job.JobService
+	Todo     *TodoService
+	Comment  *CommentService
+	Category *CategoryService
 }
 
 func NewServices(s *server.Server, repos *repository.Repositories) (*Services, error) {
 	authService := NewAuthService(s)
 
 	return &Services{
-		Job:  s.Job,
-		Auth: authService,
+		Job:      s.Job,
+		Auth:     authService,
+		Todo:     NewTodoService(s, repos.Todo, repos.Category),
+		Comment:  NewCommentService(s, repos.Comment, repos.Todo),
+		Category: NewCategoryService(s, repos.Category),
 	}, nil
 }
